ae1c86421ec3b32f83882cdb15188876a940f219
Uy Tran <uqtontran@gmail.com>
2022-05-19 20:51:07 +0200

    remove unused package

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Pages from 'vite-plugin-pages'
-import { resolve } from 'path'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    Pages({
-      dirs: 'src/pages',
-    }),
   ],
   build: {
     rollupOptions: {
