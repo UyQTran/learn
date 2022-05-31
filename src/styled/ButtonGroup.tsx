3ff01041bcb66bfd8cc754276e9f4f93da82b2e6
Uy Tran <uqtontran@gmail.com>
2022-05-31 19:34:10 +0200

    added column gap spacing from theme

--- src/styled/ButtonGroup.tsx
+++ src/styled/ButtonGroup.tsx
@@ -10,11 +10,9 @@ interface ButtonGroupProps {
 export const ButtonGroup = styled.div<ButtonGroupProps>`
   display: grid;
   grid-template-columns: repeat(${props => props.gridColumnCount.desktop}, 1fr);
-  column-gap: 1rem;
+  column-gap: ${props => props.theme.spacing(4)};
   
   @media only screen and (max-width: 900px) {
     grid-template-columns: repeat(${props => props.gridColumnCount.mobile}, 1fr);
   }
-  
-  
 `
