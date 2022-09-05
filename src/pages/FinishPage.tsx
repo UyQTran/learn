345c66e61778ce7f8d3f5ebc10f836a26dd20f51
Uy Tran <uy.tran@webstep.no>
2022-09-05 18:32:51 +0200

    replace trophy with cat

--- src/pages/FinishPage.tsx
+++ src/pages/FinishPage.tsx
@@ -1,7 +1,6 @@
 import { motion, useAnimation } from 'framer-motion'
 import Header from '../components/Header'
 import styled from 'styled-components'
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents'
 import { Typography } from '@mui/material'
 import Confetti from 'react-confetti'
 import useWindowSize from 'react-use/lib/useWindowSize'
@@ -9,27 +8,18 @@ import useWindowSize from 'react-use/lib/useWindowSize'
 
 const Congrats = styled(Typography)`
   text-align: center;
+  white-space: normal;
 `
 
-const Trophy = styled.div`
-  display: flex;
-  justify-content: center;
-  svg {
-    font-size: 30rem;
-    color: ${props => props.theme.palette.primary.main};
-    margin-right: 10rem;
-  }
+const Cat = styled.iframe`
+  margin-top: ${props => props.theme.spacing(8)};;
+  width: 100%; 
+  height: 100%;
+  border: none;
 `
-
-const Progger = styled(Typography)`
-  color: ${props => props.theme.palette.secondary.dark};
-  position: relative;
-  top: 9rem;
-  left: 20rem;
-`
-
 
 const FinishPage = () => {
+  document.body.style.overflow = 'hidden'
   const controls = useAnimation()
   const { width, height } = useWindowSize()
 
@@ -44,10 +34,7 @@ const FinishPage = () => {
         <Congrats variant="h6">
           Gratulerer! Du klarte alle oppgavene og kan nå kalle deg selv en ekte progger!
         </Congrats>
-        <Trophy>
-          <Progger variant="h3">Progger</Progger>
-          <EmojiEventsIcon/>
-        </Trophy>
+        <Cat src="https://giphy.com/embed/BK1EfIsdkKZMY"></Cat>
       </motion.div>
       <Confetti
         width={width}
